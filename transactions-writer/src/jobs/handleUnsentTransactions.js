266a267926b43f386cf037d1fd1f8d26efd5456c
Giora Guttsait <giora111@gmail.com>
2018-03-05 00:37:09 +0200

    Finish the (stubbed) structure of the Request Manager subsystem

--- transactions-writer/src/jobs/handleUnsentTransactions.js
+++ transactions-writer/src/jobs/handleUnsentTransactions.js
@@ -1,21 +1,57 @@
-const {exceptions: {UnexpectedError}, loggers: {logger}} = require('@welldone-software/node-toolbelt')
-const {utils: {updateSentRecords, getAllUnsentFromTable}} = require('stox-bc-request-manager-common')
-
 const context = require('context')
+const {exceptions: {UnexpectedError}, loggers: {logger}} = require('@welldone-software/node-toolbelt')
+const {utils: {getAllUnsentFromTable, nounceFromAccountNounces}} = require('stox-bc-request-manager-common')
 
-// just changes sentAt field for now(), need later integration with blockchain, parity node ect..
+
+const fetchNounceFromParityNode = async () => 3.14
+
+const isTransactionAlreadySigned = async ({from, network}, dbTransaction) => {
+  const nounceFromParity = await fetchNounceFromParityNode()
+  const nounceFromAccountNounce = await nounceFromAccountNounces(from, network, dbTransaction)
+  return [nounceFromAccountNounce >= nounceFromParity, nounceFromParity + 1]
+}
+
+const fetchGasPriceFromGasCalculator = async () => 3.14
+
+const signTransactionInTransactionSigner = async t => t
+
+const sendTransactionToBlockchain = async () => '123123123123123'
+
 module.exports = {
   cron: '*/05 * * * * *',
   job: async () => {
     const {db} = context
     const transaction = await db.sequelize.transaction()
     try {
-      const transactions = await getAllUnsentFromTable(db.transactions, transaction)
-      if (transactions.length) {
-        logger.debug('Transactions to be signed: ', transactions)
-        const transactionToUpdate = transactions.map(({id}) => id)
-        await updateSentRecords(db.transactions, transactionToUpdate, transaction)
-      }
+      const transactions = await getAllUnsentFromTable(db.transactions, transaction) // d.i
+
+      await Promise.all(transactions.map(async (t) => {
+        const [alreadySigned, nounce] = await isTransactionAlreadySigned(t, transaction)
+
+        if (alreadySigned) { // d.ii-iv
+          logger.debug({t}, 'transaction already signed')
+          return
+        }
+
+        const gasPrice = await fetchGasPriceFromGasCalculator(t) // d.v
+        const signedTransaction = await signTransactionInTransactionSigner(t) // d.vi
+        const tranascationHash = await sendTransactionToBlockchain(signedTransaction) // f.i
+
+        await t.update({ // f.ii
+          tranascationHash,
+          gasPrice,
+          nounce,
+          sentAt: Date.now(),
+        })
+
+        const {requestId, from, network} = t
+
+        const request = await db.requests.findOne({where: {id: requestId}})
+        await request.update({sentAt: Date.now()}, {transaction}) // f.iii
+
+        const accountNounce = await db.accountNounces.findOrCreate({where: {account: from, network}})
+        await accountNounce.update({nounce}) // f.iv
+      }))
 
       await transaction.commit()
     } catch (e) {
