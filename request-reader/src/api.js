7536fa2ebe97953bffa590de73bbc20c413bd75f
“razawi” <raz.kronenberg@gmail.com>
2018-03-07 12:06:05 +0200

    styling fix for eslint

--- request-reader/src/api.js
+++ request-reader/src/api.js
@@ -10,29 +10,29 @@ module.exports = {
     router.post(
       '/requests/',
       _(({body}) => {
-          const {requests} = createService(context)
-          return requests.createRequest(body)
+        const {requests} = createService(context)
+        return requests.createRequest(body)
       })
     )
     router.get(
       '/requests/:id',
       _(({params: {id}}) => {
-          const {requests} = createService(context)
-          return requests.getRequestById(id)
+        const {requests} = createService(context)
+        return requests.getRequestById(id)
       })
     )
     router.get(
       '/requests/transactions/:id',
       _(({params: {id}}) => {
-          const {requests} = createService(context)
-          return requests.getRequestByTransactionId(id)
+        const {requests} = createService(context)
+        return requests.getRequestByTransactionId(id)
       })
     )
     router.get(
       '/requests/:type/count',
       _(({params: {type}}) => {
-          const {requests} = createService(context)
-          return requests.countRequestByType(type)
+        const {requests} = createService(context)
+        return requests.countRequestByType(type)
       })
     )
   },
--- request-reader/src/utils/index.js
+++ request-reader/src/utils/index.js
@@ -1,11 +1,14 @@
+
+const {loggers: {logger}} = require('@welldone-software/node-toolbelt')
+
 const handleQueueMessage = handler => (error, message) => {
-    if (error) {
-        logger.error('Error consuming message from incomingRequests queue: ', error)
-    } else {
-        handler(message.body)
-    }
+  if (error) {
+    logger.error('Error consuming message from incomingRequests queue: ', error)
+  } else {
+    handler(message.body)
+  }
 }
 
 module.exports = {
-    handleQueueMessage
+  handleQueueMessage,
 }
