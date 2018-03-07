c3893a519d2e7b0768f1adba98f9823005070a6e
Elad Heller <eladheller@gmail.com>
2018-03-07 12:45:51 +0200

    fix install script

--- install.sh
+++ install.sh
@@ -3,13 +3,14 @@
 # pwd
 
 echo installing packages
+npm i
 npm install rimraf -g
 
 common="${PWD##*/}-common"
 
 cd "./common"
 echo "installing common/"
-if [ "$1" != "clean" ]
+if [ "$1" = "clean" ]
 then
     rimraf node_modules/
     rimraf package-lock.json
@@ -19,11 +20,11 @@ cd ..
 
 for i in */
 do
-    if [ "$i" != "common/" ]
+    if [[ "$i" != "common/" && "$i" != "node_modules/" ]]
     then
         cd "./$i"
         echo "installing $i"
-        if [ "$1" != "clean" ]
+        if [ "$1" = "clean" ]
         then
             rimraf node_modules/
             rimraf package-lock.json
