f7e5c29fa295874eda07d750705b2cf12d7c1056
Yair Oval <yairov@gmail.com>
2018-03-09 15:01:56 +0200

    add .env.example

--- transactions-monitor/src/config.js
+++ transactions-monitor/src/config.js
@@ -3,5 +3,5 @@ const {configs: {mapEnv}} = require('@welldone-software/node-toolbelt')
 module.exports = mapEnv({
   databaseUrl: '',
   monitorTransactionsCron: '*/5 * * * * *',
-  mqConnectionUrl:'',
+  mqConnectionUrl: '',
 })
