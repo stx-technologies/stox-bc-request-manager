cabe7fea5fa8d303634de1a683b2aaca807fbd53
Yair Oval <yairov@gmail.com>
2018-03-11 23:30:53 +0200

    remove service name from queue name

--- request-handler/src/jobs/handleUnsentRequests.js
+++ request-handler/src/jobs/handleUnsentRequests.js
@@ -9,7 +9,7 @@ const {network, handleUnsentRequestCron} = require('../config')
 // eslint-disable-next-line no-unused-vars
 const withdraw = async ({data: {userWalletAddress, amount, tokenAddress, feeAmount, feeTokenAddress}, id}) => {
   // TODO: get clear api about walletABI input and output...
-  const {body: {data, address}} = await mq.rpc('wallets-sync/walletABI', {address: userWalletAddress})
+  const {body: {data, address}} = await mq.rpc('walletABI', {address: userWalletAddress})
   return {
     requestId: id,
     type: 'send',
