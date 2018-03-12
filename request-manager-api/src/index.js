1f2fa39fa623c2bf4c3d61b24d8415527e073616
Yair Oval <yairov@gmail.com>
2018-03-12 22:57:11 +0200

    fix api not getting parameters + cleanup

--- request-manager-api/src/index.js
+++ request-manager-api/src/index.js
@@ -4,10 +4,8 @@ const {loggers: {logger}} = require('@welldone-software/node-toolbelt')
 const api = require('api')
 const {models, initContext} = require('stox-bc-request-manager-common')
 const config = require('config')
-const requireAll = require('require-all')
-const path = require('path')
 
-const {databaseUrl, mqConnectionUrl} = config
+const {databaseUrl} = config
 
 const builderFunc = (builder) => {
   builder.db(databaseUrl, models)
