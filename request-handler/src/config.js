26dd03e3362973b3f4b8ade4fbfff43f50d59891
ofermusaiwork <oferm@welldone-software.com>
2018-03-14 14:50:32 +0200

    createWallet - prepareTransactions create a api call to wallet-api

--- request-handler/src/config.js
+++ request-handler/src/config.js
@@ -3,6 +3,7 @@ const {configs: {mapEnv}} = require('@welldone-software/node-toolbelt')
 module.exports = mapEnv({
   databaseUrl: '',
   mqConnectionUrl: '',
+  walletsApiBaseUrl: '',
   network: '',
   handlePendingRequestCron: '*/5 * * * * *',
 })
--- request-handler/src/plugins/createWallet.js
+++ request-handler/src/plugins/createWallet.js
@@ -1,9 +1,14 @@
 const {context: {mq}} = require('stox-bc-request-manager-common')
-const {network} = require('../config')
+const {network, walletsApiBaseUrl} = require('../config')
+const {http} = require('stox-common')
+const clientHttp = http(walletsApiBaseUrl)
 
 // eslint-disable-next-line no-unused-vars
 const createWallet = {
   prepareTransactions: async ({id}) => {
+    //TODO: should change to the right endpoint that return the wallet transactionData
+    const count = await clientHttp.get(`/unassigned/count`)
+
     return {
       requestId: id,
       type: 'send',
