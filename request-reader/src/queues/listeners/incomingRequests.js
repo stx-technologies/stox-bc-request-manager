3ed290eaaf16c994974d7dbd9e13cda555088415
Yair Oval <yairov@gmail.com>
2018-03-14 08:25:22 +0200

    log queue message

--- request-reader/src/queues/listeners/incomingRequests.js
+++ request-reader/src/queues/listeners/incomingRequests.js
@@ -4,12 +4,13 @@ const {loggers: {logger}} = require('@welldone-software/node-toolbelt')
 module.exports = async (error, {body: message}) => {
   if (error) {
     logger.error(error, 'MESSAGE_FAILED')
-  } else {
-    try {
-      await services.requests.createRequest(message)
-      logger.info(message, 'MESSAGE_RECIEVED')
-    } catch (e) {
-      logger.error(e, 'MESSAGE_FAILED')
-    }
+    return
+  }
+
+  try {
+    await services.requests.createRequest(message)
+    logger.info(message, 'MESSAGE_RECIEVED')
+  } catch (e) {
+    logger.error(e, 'MESSAGE_FAILED')
   }
 }
