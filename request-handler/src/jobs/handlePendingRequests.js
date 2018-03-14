e736b1961c55dd77aee2c9fb543d31572f034d11
Yair Oval <yairov@gmail.com>
2018-03-14 09:18:54 +0200

    wallet-issuing

--- request-handler/src/jobs/handlePendingRequests.js
+++ request-handler/src/jobs/handlePendingRequests.js
@@ -15,21 +15,24 @@ module.exports = {
     logger.info({count: pendingRequests.length}, 'PENDING_REQUESTS_COUNT')
 
     if (pendingRequests.length) {
-      const transactionsPromises = pendingRequests.map(request => plugins[request.type](request))
-      const requestTransactions = await Promise.all(transactionsPromises)
+      pendingRequests.forEach(async (request) => {
+        const plugin = plugins[request.type]
+        const requestTransactions = plugin.prepareTransactions(request)
+        const transaction = await db.sequelize.transaction()
 
-      const transaction = await db.sequelize.transaction()
-      try {
-        await transactions.createTransactions(requestTransactions, transaction)
+        try {
+          await transactions.createTransactions(requestTransactions, transaction)
 
-        const pendingRequestsIds = pendingRequests.map(({id}) => id)
-        await updateSentRecords(db.requests, pendingRequestsIds, transaction)
+          // const pendingRequestsIds = pendingRequests.map(({id}) => id)
+          request.update({sentAt: new Date()})
+          // await updateSentRecords(db.requests, pendingRequestsIds, transaction)
 
-        await transaction.commit()
-      } catch (e) {
-        transaction.rollback()
-        throw new UnexpectedError(e)
-      }
+          await transaction.commit()
+        } catch (e) {
+          transaction.rollback()
+          throw new UnexpectedError(e)
+        }
+      })
     }
   },
 }
