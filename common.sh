87fec5bc2b50a8c14171b05936f03eb4e38de113
Yair Oval <yairov@gmail.com>
2018-03-20 12:55:13 +0200

    update 'common.sh' script

--- common.sh
+++ common.sh
@@ -6,28 +6,25 @@ echo update stox-common for all servicess
 
 for i in */
 do
-   if [ "$i" != "common/" ]
-    then
-        cd "./$i"
+    cd "./$i"
 
-        case "$1" in
-            update)     echo "updating $i"
-                        npm update stox-common
-                ;;
-            uninstall)  echo "uninstall $i"
-                        npm uninstall stox-common
-                ;;
-            install)    echo "installing $i"
-                        npm install stox-common@git+https://bitbucket.org/stx_site/stox-common.git#dev --save
-                ;;
-            link)       echo "linking $i"
-                        npm link stox-common
-                ;;
-            unlink)     echo "unlinking $i"
-                        npm unlink stox-common
-                ;;
-        esac
+    case "$1" in
+        update)     echo "updating $i"
+                    npm update stox-common
+            ;;
+        uninstall)  echo "uninstall $i"
+                    npm uninstall stox-common
+            ;;
+        install)    echo "installing $i"
+                    npm install stox-common@git+https://bitbucket.org/stx_site/stox-common.git#dev --save
+            ;;
+        link)       echo "linking $i"
+                    npm link stox-common
+            ;;
+        unlink)     echo "unlinking $i"
+                    npm unlink stox-common
+            ;;
+    esac
 
-        cd ..
-    fi
+    cd ..
 done
