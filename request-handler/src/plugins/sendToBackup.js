c76a0a976933d702994fd2a298253632601cfb97
Elad Heller <eladheller@gmail.com>
2018-03-22 15:00:34 +0200

    request plugins

--- request-handler/src/plugins/sendToBackup.js
+++ request-handler/src/plugins/sendToBackup.js
@@ -1,15 +1,15 @@
 const {network, walletsApiBaseUrl} = require('../config')
 const {http} = require('stox-common')
+const {oneline} = require('../utils')
 
 const clientHttp = http(walletsApiBaseUrl)
 
 module.exports = {
   prepareTransactions: async ({id, data: {userWithdrawalAddress, walletAddress, tokenAddress, amount}}) => {
-    const {encodedAbi, fromAccount} = await clientHttp.get('/abi/transferToBackup', {
-      walletAddress,
-      tokenAddress,
-      amount,
-    })
+    const {encodedAbi, fromAccount} = await clientHttp.get(oneline`/abi/transferToBackup?
+      walletAddress=${walletAddress}&
+      tokenAddress=${tokenAddress}&
+      amount=${amount}`)
     return [
       {
         requestId: id,
