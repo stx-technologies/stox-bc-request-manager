31337ce61d1fea354026cfb347b20e103013f449
Elad Heller <eladheller@gmail.com>
2018-03-28 09:11:04 +0300

    stox-common http get params

--- request-handler/src/plugins/sendPrize.js
+++ request-handler/src/plugins/sendPrize.js
@@ -1,16 +1,16 @@
 const {network, walletsApiBaseUrl, prizeDistributionAccountAddress} = require('../config')
 const {http} = require('stox-common')
-const {oneline} = require('../utils')
 
 const clientHttp = http(walletsApiBaseUrl)
 
 module.exports = {
   prepareTransactions: async ({id, data: {userStoxWalletAddress, amount, tokenAddress}}) => {
-    const {encodedAbi, fromAccount} = await clientHttp.get(oneline`/abi/sendPrize?
-      prizeReceiverAddress=${userStoxWalletAddress}&
-      tokenAddress=${tokenAddress}&
-      amount=${amount}&
-      prizeDistributorAddress=${prizeDistributionAccountAddress}`)
+    const {encodedAbi, fromAccount} = await clientHttp.get('/abi/sendPrize', {
+      prizeReceiverAddress: userStoxWalletAddress,
+      tokenAddress,
+      amount,
+      prizeDistributorAddress: prizeDistributionAccountAddress,
+    })
     return [
       {
         requestId: id,
