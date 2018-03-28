c67d29627a0a1a705e0e4bf8bba20710ab264f68
Elad Heller <eladheller@gmail.com>
2018-03-28 09:11:04 +0300

    stox-common http get params

--- request-handler/src/utils.js
+++
@@ -1,7 +0,0 @@
-const oneline = (strings, ...params) =>
-  strings.reduce(
-    (prev, curr, index) => prev + curr.replace(/[\s\t\r\n]/g, '') + (index in params ? params[index] : ''),
-    ''
-  )
-
-module.exports = {oneline}
