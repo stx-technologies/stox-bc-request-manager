647a9c64d80400c5ccff97f4490514f51a70000f
Danny <danny@stox.com>
2018-04-08 12:00:10 +0300

    transactions-signer: added check that we have the public key available

--- packages/transactions-signer/src/services/signer.js
+++ packages/transactions-signer/src/services/signer.js
@@ -1,8 +1,14 @@
+const {exceptions: {UnexpectedError}} = require('@welldone-software/node-toolbelt')
 const {keys} = require('config')
 const EthereumTx = require('ethereumjs-tx')
 const {context} = require('stox-bc-request-manager-common')
 
 const signTransaction = (from, unsignedTransaction, transactionId) => {
+  const publicKey = JSON.parse(keys)[from]
+
+  if (!publicKey) {
+    throw new UnexpectedError('Invalid Public Key', {from})
+  }
   const privateKey = Buffer.from(JSON.parse(keys)[from], 'hex')
   const transaction = new EthereumTx(unsignedTransaction)
   transaction.sign(privateKey)
