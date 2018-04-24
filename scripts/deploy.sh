dfc4396d878458d3ac54a62d3bb502b84d5322bf
Israel_Stox <israel@stox.com>
2018-04-24 12:16:03 +0000

    deploy to aws ecs

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,8 @@
-#!/usr/bin/env bash
 curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
-unzip awscli-bundle.zip -d /tmp/
-/tmp/awscli-bundle/install -b ~/bin/aws
-export PATH=~/bin:$PATH
-eval $(aws ecr get-login --no-include-email --region ${AWS_DEFAULT_REGION})
+unzip awscli-bundle.zip
+./awscli-bundle/install -b ~/bin/aws
+export PATH=~/bin:$PATH#
+LOGIN=$(aws ecr get-login --no-include-email --region $AWS_DEFAULT_REGION)
+echo $LOGIN
+$LOGIN
 docker push $IMAGE_NAME
