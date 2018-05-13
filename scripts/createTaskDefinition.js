bcd32c5b178821762739d444d2f784bb0949144b
Yair Oval <yairov@gmail.com>
2018-05-13 16:40:18 +0300

    update tasks definitions to use aws ssm

--- scripts/createTaskDefinition.js
+++ scripts/createTaskDefinition.js
@@ -37,8 +37,8 @@ readdirSync(tasksPath).forEach(async (d) => {
 
     const {revision} = jsonResult.taskDefinition
     const updateService = `aws ecs update-service --cluster stox-${env} --service ${family}`
-
-    const res1 = await executeCommand(`${updateService} --desired-count 0`)
+    //
+    const res1 = await executeCommand(`${updateService} --desired-count 0 --task-definition ${family}:${revision}`)
     console.log(res1)
     const res2 = await executeCommand(`${updateService} --desired-count 1 --task-definition ${family}:${revision}`)
     console.log(res2)
