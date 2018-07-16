6bbef4810e29441aba72ba6731fbdc57fe94f83f
ariel <ariel@stox.com>
2018-07-16 12:27:42 +0300

    fix types on external-transactions-handler

--- packages/external-transactions-handler/src/queues/listeners/uncompletedBlockchainTokenTransfers.js
+++ packages/external-transactions-handler/src/queues/listeners/uncompletedBlockchainTokenTransfers.js
@@ -1,6 +1,15 @@
 const {context: {db, mq}} = require('stox-bc-request-manager-common')
 const {errors: {logError}} = require('stox-common')
 
+const getType = (to, message, transactionFromDb) => {
+  switch (to) {
+    case message.feesAccount: return 'withdraw-fee'
+    case message.userWithdrawalAccount: return 'withdraw-completed'
+    case message.walletAddress:
+      return (transactionFromDb && transactionFromDb.request.type === 'sendPrize') ? 'prize' : 'deposit'
+    default: return ''
+  }
+}
 module.exports = async ({body: message}) => {
   try {
     const transactions = await Promise.all(message.transactions.map(async (transaction) => {
@@ -8,9 +17,10 @@ module.exports = async ({body: message}) => {
         where: {transactionHash: transaction.transactionHash},
         include: [{model: db.requests}],
       })
+      const type = getType(transaction.to, message, transactionFromDb)
       return transactionFromDb ?
-        {...transaction, requestId: transactionFromDb.requestId, type: transactionFromDb.request.type} :
-        {...transaction, type: message.address === transaction.from ? 'withdraw' : 'deposit'}
+        {...transaction, requestId: transactionFromDb.requestId, type} :
+        {...transaction, type}
     }))
     mq.publish('blockchain-token-transfers', {...message, transactions})
   } catch (e) {
