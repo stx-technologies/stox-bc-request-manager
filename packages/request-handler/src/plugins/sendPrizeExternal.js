57a26fef80b2862c329f7e663a59157f00c4826f
ariel <ariel@stox.com>
2018-07-25 11:49:56 +0300

    STX-1664 Add new request type: send-prize-external

---
+++ packages/request-handler/src/plugins/sendPrizeExternal.js
@@ -0,0 +1,25 @@
+const {network, walletsApiBaseUrl, prizeDistributionAccountAddress} = require('../config')
+const {http} = require('stox-common')
+
+const clientHttp = http(walletsApiBaseUrl)
+
+module.exports = {
+  prepareTransactions: async ({id, data: {userStoxWalletAddress, amount, tokenAddress}}) => {
+    const {encodedAbi, fromAccount} = await clientHttp.get('/abi/sendPrizeExternal', {
+      userStoxWalletAddress,
+      tokenAddress,
+      amount,
+      prizeDistributorAddress: prizeDistributionAccountAddress,
+    })
+    return [
+      {
+        requestId: id,
+        type: 'send',
+        from: fromAccount,
+        to: tokenAddress,
+        network,
+        transactionData: encodedAbi,
+      },
+    ]
+  },
+}
